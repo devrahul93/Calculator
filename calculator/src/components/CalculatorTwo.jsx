faa0f9c92362d66765dda49c9bf8b048f275750f
Rahul <r27m93@gmail.com>
2022-01-05 19:11:48 +0530

    added css for toggling

--- calculator/src/components/CalculatorTwo.jsx
+++ calculator/src/components/CalculatorTwo.jsx
@@ -37,150 +37,152 @@ const CalculatorTwo = () => {
 
   return (
     <>
-      <div className="container">
-        <div className="header">
-          <div>Calculator</div>
-          <div>
-            <span>Theme</span>
-            <Toggler />
+      <div className="wrapper" data-wrapper>
+        <div className="container">
+          <div className="header">
+            <div>Calculator</div>
+            <div>
+              <span>Theme</span>
+              <Toggler />
+            </div>
           </div>
-        </div>
-        <div className="screen">
-          <div className="Resultscreen">{digit || '0'}</div>
-        </div>
-        <div className="buttons">
-          <button
-            className="but"
-            onClick={() => {
-              updateDigit('7')
-            }}
-          >
-            7
-          </button>
-          <button
-            className="but"
-            onClick={() => {
-              updateDigit('8')
-            }}
-          >
-            8
-          </button>
-          <button
-            className="but"
-            onClick={() => {
-              updateDigit('9')
-            }}
-          >
-            9
-          </button>
-          <button className="but delete" onClick={backSpace}>
-            DEL
-          </button>
-          <button
-            className="but"
-            onClick={() => {
-              updateDigit('4')
-            }}
-          >
-            4
-          </button>
-          <button
-            className="but"
-            onClick={() => {
-              updateDigit('5')
-            }}
-          >
-            5
-          </button>
-          <button
-            className="but"
-            onClick={() => {
-              updateDigit('6')
-            }}
-          >
-            6
-          </button>
-          <button
-            className="but"
-            onClick={() => {
-              updateDigit('+')
-            }}
-          >
-            +
-          </button>
-          <button
-            className="but"
-            onClick={() => {
-              updateDigit('1')
-            }}
-          >
-            1
-          </button>
-          <button
-            className="but"
-            onClick={() => {
-              updateDigit('2')
-            }}
-          >
-            2
-          </button>
-          <button
-            className="but"
-            onClick={() => {
-              updateDigit('3')
-            }}
-          >
-            3
-          </button>
-          <button
-            className="but"
-            onClick={() => {
-              updateDigit('-')
-            }}
-          >
-            &minus;
-          </button>
-          <button
-            className="but"
-            onClick={() => {
-              updateDigit('.')
-            }}
-          >
-            .
-          </button>
-          <button
-            className="but"
-            onClick={() => {
-              updateDigit('0')
-            }}
-          >
-            0
-          </button>
-          <button
-            className="but"
-            onClick={() => {
-              updateDigit('/')
-            }}
-          >
-            &#8725;
-          </button>
-          <button
-            className="but"
-            onClick={() => {
-              updateDigit('*')
-            }}
-          >
-            &times;
-          </button>
-          <button
-            className="but but-1 delete"
-            onClick={() => setDigit(initialstate)}
-          >
-            RESET
-          </button>
-          <button className="but but-2" onClick={calculate}>
-            =
-          </button>
+          <div className="screen">
+            <div className="Resultscreen">{digit || '0'}</div>
+          </div>
+          <div className="buttons">
+            <button
+              className="but"
+              onClick={() => {
+                updateDigit('7')
+              }}
+            >
+              7
+            </button>
+            <button
+              className="but"
+              onClick={() => {
+                updateDigit('8')
+              }}
+            >
+              8
+            </button>
+            <button
+              className="but"
+              onClick={() => {
+                updateDigit('9')
+              }}
+            >
+              9
+            </button>
+            <button className="but delete" onClick={backSpace}>
+              DEL
+            </button>
+            <button
+              className="but"
+              onClick={() => {
+                updateDigit('4')
+              }}
+            >
+              4
+            </button>
+            <button
+              className="but"
+              onClick={() => {
+                updateDigit('5')
+              }}
+            >
+              5
+            </button>
+            <button
+              className="but"
+              onClick={() => {
+                updateDigit('6')
+              }}
+            >
+              6
+            </button>
+            <button
+              className="but"
+              onClick={() => {
+                updateDigit('+')
+              }}
+            >
+              +
+            </button>
+            <button
+              className="but"
+              onClick={() => {
+                updateDigit('1')
+              }}
+            >
+              1
+            </button>
+            <button
+              className="but"
+              onClick={() => {
+                updateDigit('2')
+              }}
+            >
+              2
+            </button>
+            <button
+              className="but"
+              onClick={() => {
+                updateDigit('3')
+              }}
+            >
+              3
+            </button>
+            <button
+              className="but"
+              onClick={() => {
+                updateDigit('-')
+              }}
+            >
+              &minus;
+            </button>
+            <button
+              className="but"
+              onClick={() => {
+                updateDigit('.')
+              }}
+            >
+              .
+            </button>
+            <button
+              className="but"
+              onClick={() => {
+                updateDigit('0')
+              }}
+            >
+              0
+            </button>
+            <button
+              className="but"
+              onClick={() => {
+                updateDigit('/')
+              }}
+            >
+              &#8725;
+            </button>
+            <button
+              className="but"
+              onClick={() => {
+                updateDigit('*')
+              }}
+            >
+              &times;
+            </button>
+            <button
+              className="but but-1 delete"
+              onClick={() => setDigit(initialstate)}
+            >
+              RESET
+            </button>
+            <button className="but but-2" onClick={calculate}>
+              =
+            </button>
+          </div>
         </div>
       </div>
     </>
